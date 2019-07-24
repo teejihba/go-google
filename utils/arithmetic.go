f2156384afedf21fb511de1783585399b3420e60
Abhijeet Ankush <abhijeet.ankush@swiggy.in>
2019-07-24 19:37:50 +0530

    added variadic min and max functions

--- utils/arithmetic.go
+++ utils/arithmetic.go
@@ -1,5 +1,7 @@
 package utils
 
+import "math"
+
 func Max(i1 int, i2 int) int {
 	if i1>=i2 {
 		return i1
@@ -11,4 +13,20 @@ func Min(i1 int, i2 int) int {
 		return i2
 	}
 	return i1
+}
+
+func MinVarArg(nums ...int) int {
+	min := math.MaxInt64
+	for _, num := range nums {
+		min = Min(min, num)
+	}
+	return min
+}
+
+func MaxVarArg(nums ...int) int {
+	max := math.MinInt64
+	for _, num := range nums {
+		max = Max(max, num)
+	}
+	return max
 }
